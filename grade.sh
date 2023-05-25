7156c27257e31120254babf257e8527c08ad1378
kqngyn <khn037@ucsd.edu>
2023-05-25 14:33:09 -0700

    m

--- grade.sh
+++ grade.sh
@@ -8,6 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+files=`find *ListExamples.java`
+if [[ -f $files ]] && [[ -e $files ]]
+    then
+        echo "File found: " $files
+    else
+        echo "File not found."
+        exit
+fi
+
+cp -r student-submission/ListExamples.java ./grading-area
+echo grading-area/*
+
+javac -cp $CPATH grading-area/*.java
+
+# Compile your tests and the student’s code from the appropriate directory with the appropriate classpath commands. 
+# If the compilation fails, detect and give helpful feedback about it.
+if [[ -f $files ]]
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
